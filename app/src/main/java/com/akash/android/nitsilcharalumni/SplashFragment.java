e9a525f78927497a67fa19e92960026bf017d782
akashnits <akash.nits@outlook.com>
2017-09-18 15:14:26 +0530

    Initial commit

---
+++ app/src/main/java/com/akash/android/nitsilcharalumni/SplashFragment.java
@@ -0,0 +1,80 @@
+package com.akash.android.nitsilcharalumni;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SplashFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SplashFragment extends Fragment {
+
+    private static int SPLASH_TIME_OUT = 3000;
+    @BindView(R.id.pbSplashScreen)
+    ProgressBar pbSplashScreen;
+    Unbinder unbinder;
+
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SplashFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SplashFragment newInstance() {
+        SplashFragment fragment = new SplashFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        pbSplashScreen.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            pbSplashScreen.setVisibility(View.GONE);
+                Toast.makeText(getActivity(), "Launch LoginActivity", Toast.LENGTH_SHORT).show();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
