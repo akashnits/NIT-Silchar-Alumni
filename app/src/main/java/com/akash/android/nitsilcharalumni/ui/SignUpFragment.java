724e48315137d3e78dd90eab17fcca4c79fad8d0
akashnits <akash.nits@outlook.com>
2017-09-21 19:27:59 +0530

    Sign Up continued

--- app/src/main/java/com/akash/android/nitsilcharalumni/ui/SignUpFragment.java
+++ app/src/main/java/com/akash/android/nitsilcharalumni/ui/SignUpFragment.java
@@ -1,22 +1,70 @@
 package com.akash.android.nitsilcharalumni.ui;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.akash.android.nitsilcharalumni.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SignUpFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SignUpFragment extends Fragment {
+public class SignUpFragment extends Fragment   {
 
 
+    @BindView(R.id.edit_text_username_create)
+    EditText editTextUsernameCreate;
+    @BindView(R.id.til_username_create)
+    TextInputLayout tilUsernameCreate;
+    @BindView(R.id.edit_text_email_create)
+    EditText editTextEmailCreate;
+    @BindView(R.id.til_email_create)
+    TextInputLayout tilEmailCreate;
+    @BindView(R.id.edit_text_password_create_1)
+    EditText editTextPasswordCreate1;
+    @BindView(R.id.til_password_create)
+    TextInputLayout tilPasswordCreate;
+    @BindView(R.id.edit_text_password_create_2)
+    EditText editTextPasswordCreate2;
+    @BindView(R.id.til_password_recreate)
+    TextInputLayout tilPasswordRecreate;
+    @BindView(R.id.radioButtonMale)
+    RadioButton radioButtonMale;
+    @BindView(R.id.radioButtonFemale)
+    RadioButton radioButtonFemale;
+    @BindView(R.id.tvTypeofUser)
+    TextView tvTypeofUser;
+    @BindView(R.id.spinner)
+    Spinner spinner;
+    @BindView(R.id.btn_create_account_final)
+    Button btnCreateAccountFinal;
+    @BindView(R.id.tv_already_have_account)
+    TextView tvAlreadyHaveAccount;
+    @BindView(R.id.tv_sign_in)
+    TextView tvSignIn;
+    @BindView(R.id.linear_layout_create_account_activity)
+    LinearLayout linearLayoutCreateAccountActivity;
+    Unbinder unbinder;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -25,6 +73,7 @@ public class SignUpFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
+     *
      * @return A new instance of fragment SignUpFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -42,7 +91,36 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.typeOfUser, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView) adapterView.getChildAt(0)).setTextColor(Color.WHITE);
+                ((TextView) adapterView.getChildAt(0)).setTextSize(20);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
