325b10022705a6b3fea9561fb712194d1e977f7a
akash.nits <akash.nits@outlook.com>
2017-09-21 08:53:17 +0530

    SignUp

--- app/src/main/java/com/akash/android/nitsilcharalumni/ui/LoginFragment.java
+++ app/src/main/java/com/akash/android/nitsilcharalumni/ui/LoginFragment.java
@@ -1,13 +1,25 @@
 package com.akash.android.nitsilcharalumni.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.akash.android.nitsilcharalumni.R;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.SignInButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +29,33 @@ import com.akash.android.nitsilcharalumni.R;
 public class LoginFragment extends Fragment {
 
 
+    @BindView(R.id.login_button)
+    LoginButton loginButton;
+    @BindView(R.id.tvForgetPassword)
+    TextView tvForgetPassword;
+    @BindView(R.id.btSignIn)
+    Button btSignIn;
+    @BindView(R.id.tvEmail)
+    TextView tvEmail;
+    @BindView(R.id.tvPassword)
+    TextView tvPassword;
+    @BindView(R.id.tvOR)
+    TextView tvOR;
+    @BindView(R.id.sign_in_button)
+    SignInButton signInButton;
+    @BindView(R.id.etEmail)
+    TextInputEditText etEmail;
+    @BindView(R.id.tilEmail)
+    TextInputLayout tilEmail;
+    @BindView(R.id.etPassword)
+    TextInputEditText etPassword;
+    @BindView(R.id.tilPassword)
+    TextInputLayout tilPassword;
+    @BindView(R.id.tvNewUser)
+    TextView tvNewUser;
+    @BindView(R.id.tvSignUp)
+    TextView tvSignUp;
+    Unbinder unbinder;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -45,7 +84,26 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
---
+++ app/src/main/java/com/akash/android/nitsilcharalumni/ui/SignUpFragment.java
@@ -0,0 +1,48 @@
+package com.akash.android.nitsilcharalumni.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.akash.android.nitsilcharalumni.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SignUpFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SignUpFragment extends Fragment {
+
+
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment SignUpFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SignUpFragment newInstance(String param1, String param2) {
+        SignUpFragment fragment = new SignUpFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+}
