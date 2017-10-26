28fa50492494590d8f8b50685304801d990441e0
akash.nits <akash.nits@outlook.com>
2017-10-27 03:12:16 +0530

    TODO: Fix Current location feature

--- app/src/main/java/com/akash/android/nitsilcharalumni/ui/fragments/SignUpFragment.java
+++ app/src/main/java/com/akash/android/nitsilcharalumni/ui/fragments/SignUpFragment.java
@@ -1,13 +1,25 @@
 package com.akash.android.nitsilcharalumni.ui.fragments;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -18,14 +30,25 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.akash.android.nitsilcharalumni.BuildConfig;
 import com.akash.android.nitsilcharalumni.R;
 import com.akash.android.nitsilcharalumni.ui.activities.LoginActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+
+import static com.akash.android.nitsilcharalumni.R.id.edit_text_choose_location;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -67,7 +90,25 @@ public class SignUpFragment extends Fragment {
     TextView tvSignIn;
     @BindView(R.id.linear_layout_create_account_activity)
     LinearLayout linearLayoutCreateAccountActivity;
+    @BindView(R.id.tvSignUp)
+    TextView tvSignUp;
+    @BindView(edit_text_choose_location)
+    EditText editTextChooseLocation;
+    @BindView(R.id.til_choose_location)
+    TextInputLayout tilChooseLocation;
     Unbinder unbinder;
+
+
+    public static final String TAG= SignUpFragment.class.getSimpleName();
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    /**
+     * Provides the entry point to the Fused Location Provider API.
+     */
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    protected Location mLastLocation;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -95,12 +136,13 @@ public class SignUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
         unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.typeOfUser, android.R.layout.simple_spinner_item);
@@ -119,6 +161,152 @@ public class SignUpFragment extends Fragment {
             }
         });
         spinner.setAdapter(adapter);
+
+        editTextChooseLocation.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (editTextChooseLocation.getRight() -
+                            editTextChooseLocation.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                        if (!checkPermissions()) {
+                            requestPermissions();
+                        } else {
+                            getLastLocation(view);
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getLastLocation(final View view) {
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Log.v(TAG, task.isSuccessful()+ "");
+                        Log.v(TAG, task.getResult() + "");
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            mLastLocation = task.getResult();
+                            double lat= mLastLocation.getLatitude();
+                            double lng= mLastLocation.getLongitude();
+
+                            Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
+                            try {
+                                List<Address> addresses = gcd.getFromLocation(lat, lng, 1);
+                                if (addresses.size() > 0) {
+                                    editTextChooseLocation.setText(addresses.get(0).getLocality());
+                                }
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        } else {
+                            Log.w(TAG, "getLastLocation:exception", task.getException());
+                            Snackbar.make(view, "Please turn on location services",Snackbar.LENGTH_SHORT ).show();
+                    }
+                    }
+                });
+    }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            startLocationPermissionRequest();
+                        }
+                    });
+
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            startLocationPermissionRequest();
+        }
+    }
+
+    /**
+     * Shows a {@link Snackbar}.
+     *
+     * @param mainTextStringId The id for the string resource for the Snackbar text.
+     * @param actionStringId   The text of the action item.
+     * @param listener         The listener associated with the Snackbar action.
+     */
+    private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(getActivity().findViewById(android.R.id.content),
+                getString(mainTextStringId),
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener).show();
+    }
+
+    private void startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getLastLocation(getActivity().findViewById(R.id.linear_layout_create_account_activity));
+            } else {
+                // Permission denied.
+                showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Build intent that displays the App settings screen.
+                                Intent intent = new Intent();
+                                intent.setAction(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package",
+                                        BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
+            }
+        }
     }
 
     @Override
