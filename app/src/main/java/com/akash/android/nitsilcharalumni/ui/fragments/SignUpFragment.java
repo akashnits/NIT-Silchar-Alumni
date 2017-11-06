f56494d0f5b7c1297d287e7345f219e5dbf29242
Akash <akash.nits@outlook.com>
2017-11-06 12:07:48 +0530

    test

--- app/src/main/java/com/akash/android/nitsilcharalumni/ui/fragments/SignUpFragment.java
+++ app/src/main/java/com/akash/android/nitsilcharalumni/ui/fragments/SignUpFragment.java
@@ -122,7 +122,6 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
